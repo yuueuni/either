bc3a8eaf06ef0fbc9ff8d66e67d1e251f528ef58
yoon <yuueuni@gmail.com>
2020-04-27 16:44:21 +0900

    Votes Model - Vote, Comment

--- votes/models.py
+++ votes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Vote(models.Model):
+    title = models.CharField(max_length=200)
+    issue_a = models.CharField(max_length=500)
+    issue_b = models.CharField(max_length=500)
+
+
+class Comment(models.Model):
+    AGREE = 'AG'
+    DISAGREE = 'DAG'
+    PICK_CHOICES = (
+        (AGREE, 'Agree'),
+        (DISAGREE, 'Disagree')
+    )
+    pick = models.CharField(max_length=2, choices=PICK_CHOICES, default=AGREE)
+    content = models.CharField(max_length=200)
+    vote = models.ForeignKey(Vote, on_delete=models.CASCADE, related_name='comments')
